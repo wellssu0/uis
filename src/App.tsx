014ef08ac8a50f54605b15bf2c27cd3dbdc758d9
wellssu <wellssu0@gmail.com>
2020-08-30 23:50:59 +0800

    feat: Add basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from 'react';
 
-function App() {
+import Button, { ButtonType, ButtonSize } from './components/Button/Button';
+
+const App: FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Small}>
+        取消
+      </Button>
+      <Button btnType={ButtonType.Link} size={ButtonSize.Small} href={'google.com'} disabled>
+        Google.com
+      </Button>
+      <header className="App-header">hello world</header>
     </div>
   );
-}
+};
 
 export default App;
